92b26fc1bf0b3d378fe2da05003deadc719e3b32
deborah mizrahi <debimizfel@gmail.com>
2023-06-09 00:18:38 +0300

    project

--- Projects/Project-14-06_Deborah_Mizrahi/script.js
+++ Projects/Project-14-06_Deborah_Mizrahi/script.js
@@ -1,6 +1,4 @@
-
 function add_task() {
-
     const valid = validation();
     if (!valid) return;
 
@@ -9,9 +7,11 @@ function add_task() {
     create_postit(task);
 }
 
+
 function validation() {
     return validate_not_empty("date_err", "date_box", "Date") && validate_not_empty("time_err", "time_box", "Time") && validate_task();
 }
+
 
 function validate_not_empty(err_id, field_id, label) {
     const err_element = document.getElementById(err_id);
@@ -27,8 +27,8 @@ function validate_not_empty(err_id, field_id, label) {
     return true;
 }
 
-function validate_task() {
 
+function validate_task() {
     const textarea_box = document.getElementById("textarea_box");
     const textarea = textarea_box.value;
 
@@ -42,8 +42,8 @@ function validate_task() {
     textarea_box.placeholder = "Type over here";
     textarea_box.style.setProperty('--placeholder-color', "grey");
     return true;
-
 }
+
 
 function get_task_from_form() {
     const date = document.getElementById("date_box").value;
@@ -53,22 +53,23 @@ function get_task_from_form() {
     return create_task(date, time, task);
 }
 
+
 function create_task(date, time, task) {
-    const tasks = get_tasks_from_storage()
     return {
         date: date,
         time: time,
         task: task,
-        random: Math.floor(Math.random() * 6) + 1,
         id: Math.floor(Math.random() * 10000000) + 1
     };
 }
+
 
 function save_task(task) {
     const tasks = get_tasks_from_storage();
     tasks.push(task);
     save_tasks_to_storage(tasks);
 }
+
 
 function get_tasks_from_storage() {
     const task_JSON = localStorage.getItem("tasks");
@@ -77,15 +78,17 @@ function get_tasks_from_storage() {
     return tasks;
 }
 
+
 function save_tasks_to_storage(tasks) {
     const task_str = JSON.stringify(tasks);
     localStorage.setItem("tasks", task_str);
 }
 
+
 function create_postit(task, animate = true) {
     const postit_container = document.getElementById("postit_container");
     var div = document.createElement('div');
-    div.classList.add('postit', `postit-${task.random}`);
+    div.classList.add('postit', `postit-${Math.floor(Math.random() * 6) + 1}`);
     if (animate) {
         div.classList.add('animated');
     }
@@ -99,19 +102,22 @@ function create_postit(task, animate = true) {
     postit_container.appendChild(div);
 }
 
+
 function display_postits(tasks) {
     document.getElementById("postit_container").innerHTML = "";
 
     for (const task of tasks) {
-        create_postit(task, animate = false); //?
+        create_postit(task, animate = false);
     }
 }
+
 
 function delete_postit(id) {
     let tasks = get_tasks_from_storage().filter((task) => task.id !== id);
     save_tasks_to_storage(tasks);
     display_postits(tasks);
 }
+
 
 function clear_form() {
     document.getElementById("date_box").value = "";
@@ -120,12 +126,12 @@ function clear_form() {
     document.getElementById("date_box").focus();
 }
 
+
 function load_tasks() {
     const tasks = get_tasks_from_storage();
     display_postits(tasks);
 }
 
-load_tasks();
 
 function clear_board() {
     const tasks = get_tasks_from_storage();
@@ -136,6 +142,7 @@ function clear_board() {
     clear_form();
 }
 
+load_tasks();
 
 
 
