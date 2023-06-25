e8e58710d07e994174f80dd42408f9f5859672e8
deborah mizrahi <debimizfel@gmail.com>
2023-06-25 17:35:06 +0300

    deleted

--- in-class/06-21_/01_iife/script.js
+++
@@ -1,6 +0,0 @@
-//^ esto evita que alguien use el mismo nombre para otra funcion o vairable y cambie el valor o funcionamiento
-(() => {
-    function hello_wolrd() {
-        alert('hello world');
-    }
-})();
